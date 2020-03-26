ca2b604556ab3705e342fa481af9e72bd6738e7e
Alex Nevpryaga <alexnevpryaga@cloud.upwork.com>
2020-03-27 01:52:38 +0200

    cover screenshot file to directory when upload to IPFS in order to see readable file name in url; add IPFS_Screenshot.app (mac Automator app) for testing purposes

--- ipfs-screen.sh
+++ ipfs-screen.sh
@@ -1,22 +1,64 @@
-#! /bin/bash
+#!/bin/bash
+
+####
+#START
+#goo.gl API key
+google_api_key=AIzaSyAdr9yOCVcvtPJHA4VkX2gqnmcjXHQosWw
+
+# Shorten a URL using the Google URL Shortener service (http://goo.gl).
+goo_gl() {
+    curl -qsSL -m10 --connect-timeout 10 \
+        "https://www.googleapis.com/urlshortener/v1/url?key=$google_api_key" \
+        -H 'Content-Type: application/json' \
+        -d '{"longUrl": "'$1'"}' \
+    | \
+    perl -ne 'if(m/^\s*"id":\s*"(.*)",?$/i) { print $1 }'
+}
+#END
+####
+
 mkdir -p ~/.ipfs-screen/
-echo "testing" >> ~/.ipfs-screen/ipfs-add.log
-file=$(date +"%Y-%m-%d-%T-screenshot")
+file=$(`which date` +"%Y%m%d_%H%M%S_screenshot.jpg")
 clipboard_command="xclip -selection clipboard"
 
 platform=`uname`
 if [[ "$platform" == "Darwin" ]]
 then
-  screencapture -t jpg -i ~/.ipfs-screen/$file
+  screencapture -t jpg -i "${HOME}/.ipfs-screen/$file"
   clipboard_command="pbcopy"
 else
-  gnome-screenshot -a -f ~/.ipfs-screen/$file
+  gnome-screenshot -a -f "${HOME}/.ipfs-screen/$file"
 fi
 
-hash_log=$(ipfs add ~/.ipfs-screen/$file)
+[ -f "${HOME}/.ipfs-screen/$file" ] || exit 0
 
-echo $hash_log >> ~/.ipfs-screen/ipfs-add.log
+#hash=$(cat "${HOME}/.ipfs-screen/$file" | `which ipfs` add --pin=false -w -q | `which tr` -s '[:space:]' ' ')
+hash=$(cat "${HOME}/.ipfs-screen/$file" | `which ipfs` add --stdin-name=$file --pin=false -w --progress=false |awk 'NR==2{print $2"/"}'| { read h; echo $h''$file; })
+#hash=$(cat "${HOME}/.ipfs-screen/$file" | `which ipfs` add --stdin-name=$file --pin=false -w --progress=false |awk 'NR==1{print $2"/"$3}')
+[ ! $hash ] && exit 0
 
-hash=$(echo $hash_log | awk '{ print $2; }')
+rm "${HOME}/.ipfs-screen/$file"
 
-echo -n "https://ipfs.io/ipfs/$hash" | $clipboard_command
+ipfs_link="https://gateway.ipfs.io/ipfs/$hash"
+
+short_url=$(goo_gl $ipfs_link)
+url=$([ $short_url ] && echo $short_url || echo $ipfs_link)
+
+echo "File ${HOME}/.ipfs-screen/$file added... as $hash ($url)" >> ~/.ipfs-screen/ipfs-add.log
+
+echo -n $url | $clipboard_command > /dev/null
+
+echo $url
+
+proxychains4 curl $url &
+
+#play sound 
+if [ -t 1 ]
+then
+    /usr/bin/osascript -e "display notification \"$url\" with title \"Screenshot created\""
+    tput bel
+else
+    afplay "/System/Library/Sounds/Glass.aiff"
+fi
+
+exit 0
